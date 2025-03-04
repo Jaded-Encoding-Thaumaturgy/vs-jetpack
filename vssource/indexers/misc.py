05d55d39878a6bee60eff7b9bb7cc00b83babdc6
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-03-05 00:04:14 +0100

    BestSource: set default cachemode to 3

--- vssource/indexers/misc.py
+++ vssource/indexers/misc.py
@@ -22,6 +22,35 @@ class BestSource(Indexer):
 
     _source_func = core.lazy.bs.VideoSource
 
+    class CacheMode(CustomIntEnum):
+        """Cache mode"""
+
+        NEVER = 0
+        """Never read or write index to disk."""
+        CACHE_PATH = 1
+        """
+        Always try to read index but only write index to disk when it will make a noticeable difference
+        on subsequent runs and store index files in a subtree of *cachepath*.
+        """
+        CACHE_PATH_WRITE = 2
+        """
+        Always try to read and write index to disk and store index files in a subtree of *cachepath*.
+        """
+        ABSOLUTE = 3
+        """
+        Always try to read index but only write index to disk when it will make a noticeable difference
+        on subsequent runs and store index files in the absolute path in *cachepath*
+        with track number and index extension appended.
+        """
+        ABSOLUTE_WRITE = 4
+        """
+        Always try to read and write index to disk and store index files
+        in the absolute path in *cachepath* with track number and index extension appended.
+        """
+
+    def __init__(self, *, force: bool = True, **kwargs: Any) -> None:
+        kwargs.setdefault("cachemode", BestSource.CacheMode.ABSOLUTE)
+        super().__init__(force=force, **kwargs)
 
 class IMWRI(Indexer):
     """ImageMagick Writer-Reader indexer"""
