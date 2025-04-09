b531701ef9bb459b940669018f789c979a0e1221
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-04-09 06:22:10 +0200

    add docstrings

--- vsaa/antialiasers/nnedi3.py
+++ vsaa/antialiasers/nnedi3.py
@@ -1,3 +1,7 @@
+"""
+This module implements wrappers for the Neural Network Edge Directed Interpolation (3rd gen.)
+"""
+
 from __future__ import annotations
 
 from dataclasses import dataclass
@@ -18,13 +22,66 @@ __all__ = [
 
 @dataclass
 class NNEDI3(_FullInterpolate, _Antialiaser):
+    """Base class for NNEDI3 interpolating methods."""
+
     nsize: int = 0
+    """
+    Size of the local neighbourhood around each pixel used by the predictor neural network.
+    Possible settings:
+    - 0: 8x6
+    - 1: 16x6
+    - 2: 32x6
+    - 3: 48x6
+    - 4: 8x4
+    - 5: 16x4
+    - 6: 32x4
+    """
+
     nns: int = 4
+    """
+    Number of neurons in the predictor neural network. Possible values:
+    - 0: 16
+    - 1: 32
+    - 2: 64
+    - 3: 128
+    - 4: 256
+    """
+
     qual: int = 2
+    """
+    The number of different neural network predictions that are blended together to compute the final output value.
+    Each neural network was trained on a different set of training data.
+    Blending the results of these different networks improves generalisation to unseen data.
+    Possible values are 1 and 2.
+    """
+
     etype: int = 0
+    """
+    The set of weights used in the predictor neural network. Possible values:
+    - 0: Weights trained to minimise absolute error.
+    - 1: Weights trained to minimise squared error.
+    """
+
     pscrn: int = 1
+    """
+    The prescreener used to decide which pixels should be processed by the predictor neural network,
+    and which can be handled by simple cubic interpolation.
+    Since most pixels can be handled by cubic interpolation, using the prescreener
+    generally results in much faster processing. Possible values:
+    - 0: No prescreening. No pixels will be processed with cubic interpolation. This is really slow.
+    - 1: Old prescreener.
+    - 2: New prescreener level 0.
+    - 3: New prescreener level 1.
+    - 4: New prescreener level 2.
+
+    The new prescreener is not available with float input.
+    """
 
     opencl: bool = False
+    """
+    Enables the use of the OpenCL variant.
+    Note that this will only work if full interpolation can be performed.
+    """
 
     # Class Variable
     _shift = 0.5
@@ -62,6 +119,8 @@ class NNEDI3(_FullInterpolate, _Antialiaser):
 
 
 class Nnedi3SS(NNEDI3, SuperSampler):
+    """Concrete implementation of NNEDI3 used as a supersampler."""
+    
     @inject_self.cached.property
     def kernel_radius(self) -> int:
         match self.nsize:
@@ -76,12 +135,12 @@ class Nnedi3SS(NNEDI3, SuperSampler):
 
 
 class Nnedi3SR(NNEDI3, SingleRater):
-    ...
+    """Concrete implementation of NNEDI3 used as a single-rater."""
 
 
 class Nnedi3DR(Nnedi3SR, DoubleRater):
-    ...
+    """Concrete implementation of NNEDI3 used as a double-rater."""
 
 
 class Nnedi3(Nnedi3DR, Nnedi3SS, Antialiaser):
-    ...
+    """Full implementation of the NNEDI3 anti-aliaser"""
--- vsaa/antialiasers/sangnom.py
+++ vsaa/antialiasers/sangnom.py
@@ -1,3 +1,7 @@
+"""
+This module implements wrappers for the SangNom single field deinterlacer using edge-directed interpolation
+"""
+
 from __future__ import annotations
 
 from dataclasses import dataclass
@@ -18,8 +22,18 @@ __all__ = [
 
 @dataclass
 class SANGNOM(_Antialiaser):
+    """Base class for SANGNOM interpolating methods."""
+
     aa_strength: int | Sequence[int] = 48
+    """
+    The strength of luma anti-aliasing, applied to an 8-bit clip.
+    Must be an integer between 0 and 128, inclusive.
+    """
+
     double_fps: bool = False
+    """
+    Whether to double the frame rate of the clip.
+    """
 
     # Class Variable
     _shift = -0.5
@@ -41,16 +55,18 @@ class SANGNOM(_Antialiaser):
 
 
 class SangNomSS(SANGNOM, SuperSampler):
+    """Concrete implementation of SANGNOM used as a supersampler."""
+
     _static_kernel_radius = 2
 
 
 class SangNomSR(SANGNOM, SingleRater):
-    ...
+    """Concrete implementation of SANGNOM used as a single-rater."""
 
 
 class SangNomDR(SangNomSR, DoubleRater):
-    ...
+    """Concrete implementation of SANGNOM used as a double-rater."""
 
 
 class SangNom(SangNomDR, SangNomSS, Antialiaser):
-    ...
+    """Full implementation of the SANGNOM anti-aliaser"""
