1a819c41947c2c2f9145da647023830fdb041677
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-04-21 18:54:28 +0200

    remove replace_ranges alias & use a class decorator instead

--- vstools/utils/ranges.py
+++ vstools/utils/ranges.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
 import contextlib
-from typing import Callable, Literal, Protocol, Sequence, TypeGuard, TypeVar, Union, overload
+from typing import Any, Callable, Generic, Literal, Protocol, Sequence, TypeGuard, TypeVar, Union, overload
 
 import vapoursynth as vs
-from jetpytools import CustomValueError, flatten, interleave_arr, ranges_product
+from jetpytools import CustomValueError, P, R, flatten, interleave_arr, ranges_product
 
 from ..functions import check_ref_clip
 from ..types import ConstantFormatVideoNode, FrameRangeN, FrameRangesN, VideoNodeT
@@ -64,190 +64,88 @@ def _is_cb_n(cb: Callable[..., bool], params: set[str]) -> TypeGuard[_RangesCall
     return "n" in params
 
 
-@overload
-def replace_ranges(
-    clip_a: vs.VideoNode,
-    clip_b: vs.VideoNode,
-    ranges: FrameRangeN | FrameRangesN,
-    exclusive: bool = False,
-    mismatch: Literal[False] = ...,
-) -> ConstantFormatVideoNode:
+class ReplaceRanges(Generic[P, R]):
     """
-    Replaces frames in a clip with pre-calculated indices.
-    Frame ranges are inclusive. This behaviour can be changed by setting `exclusive=True`.
+    Class decorator that wraps the [replace_ranges][vstools.utils.replace_ranges] function
+    and extends its functionality.
 
-    Examples with clips ``black`` and ``white`` of equal length:
-        * ``replace_ranges(black, white, [(0, 1)])``: replace frames 0 and 1 with ``white``
-        * ``replace_ranges(black, white, [(None, None)])``: replace the entire clip with ``white``
-        * ``replace_ranges(black, white, [(0, None)])``: same as previous
-        * ``replace_ranges(black, white, [(200, None)])``: replace 200 until the end with ``white``
-        * ``replace_ranges(black, white, [(200, -1)])``: replace 200 until the end with ``white``,
-                                                         leaving 1 frame of ``black``
-
-    Optional Dependencies:
-        * `vs-zip <https://github.com/dnjulek/vapoursynth-zip>`_ (highly recommended!)
-
-    :param clip_a:      Original clip.
-    :param clip_b:      Replacement clip.
-    :param ranges:      Ranges to replace clip_a (original clip) with clip_b (replacement clip).
-                        Integer values in the list indicate single frames,
-                        Tuple values indicate inclusive ranges.
-                        Negative integer values will be wrapped around based on clip_b's length.
-                        None values are context dependent:
-                            * None provided as sole value to ranges: no-op
-                            * Single None value in list: Last frame in clip_b
-                            * None as first value of tuple: 0
-                            * None as second value of tuple: Last frame in clip_b
-    :param exclusive:   Use exclusive ranges (Default: False).
-    :param mismatch:    Accept format or resolution mismatch between clips.
-
-    :return:            Clip with ranges from clip_a replaced with clip_b.
+    It is not meant to be used directly.
     """
 
-
-@overload
-def replace_ranges(
-    clip_a: VideoNodeT,
-    clip_b: VideoNodeT,
-    ranges: FrameRangeN | FrameRangesN,
-    exclusive: bool = False,
-    mismatch: bool = ...,
-) -> VideoNodeT:
+    exclusive: bool | None
     """
-    Replaces frames in a clip with pre-calculated indices.
-    Frame ranges are inclusive. This behaviour can be changed by setting `exclusive=True`.
-
-    Examples with clips ``black`` and ``white`` of equal length:
-        * ``replace_ranges(black, white, [(0, 1)])``: replace frames 0 and 1 with ``white``
-        * ``replace_ranges(black, white, [(None, None)])``: replace the entire clip with ``white``
-        * ``replace_ranges(black, white, [(0, None)])``: same as previous
-        * ``replace_ranges(black, white, [(200, None)])``: replace 200 until the end with ``white``
-        * ``replace_ranges(black, white, [(200, -1)])``: replace 200 until the end with ``white``,
-                                                         leaving 1 frame of ``black``
-
-    Optional Dependencies:
-        * `vs-zip <https://github.com/dnjulek/vapoursynth-zip>`_ (highly recommended!)
-
-    :param clip_a:      Original clip.
-    :param clip_b:      Replacement clip.
-    :param ranges:      Ranges to replace clip_a (original clip) with clip_b (replacement clip).
-                        Integer values in the list indicate single frames,
-                        Tuple values indicate inclusive ranges.
-                        Negative integer values will be wrapped around based on clip_b's length.
-                        None values are context dependent:
-                            * None provided as sole value to ranges: no-op
-                            * Single None value in list: Last frame in clip_b
-                            * None as first value of tuple: 0
-                            * None as second value of tuple: Last frame in clip_b
-    :param exclusive:   Use exclusive ranges (Default: False).
-    :param mismatch:    Accept format or resolution mismatch between clips.
-
-    :return:            Clip with ranges from clip_a replaced with clip_b.
+    Whether to use exclusive ranges globally (Default: None).
+    If set to True, all calls of replace_ranges will use exclusive ranges.
+    If set to False, all calls of replace_ranges will use inclusive ranges.
     """
 
+    def __init__(self, replace_ranges: Callable[P, R]) -> None:
+        self._func = replace_ranges
+        self.exclusive = None
 
-@overload
-def replace_ranges(
-    clip_a: vs.VideoNode, clip_b: vs.VideoNode, ranges: _RangesCallBack, *, mismatch: bool = False
-) -> vs.VideoNode:
-    """
-    Replaces frames in a clip on-the-fly with a callback.
+    @overload
+    def __call__(
+        self,
+        clip_a: vs.VideoNode,
+        clip_b: vs.VideoNode,
+        ranges: FrameRangeN | FrameRangesN,
+        exclusive: bool | None = None,
+        mismatch: Literal[False] = ...,
+    ) -> ConstantFormatVideoNode: ...
 
-    Example usage:
-    ```py
-    # Replace frames from `clip_a` with `clip_b` if the frame number is between 200 and 299 (inclusive).
-    clip = replace_ranges(clip_a, clip_b, lambda n: n in range(200, 300))
-    ```
+    @overload
+    def __call__(
+        self,
+        clip_a: VideoNodeT,
+        clip_b: VideoNodeT,
+        ranges: FrameRangeN | FrameRangesN,
+        exclusive: bool | None = None,
+        mismatch: Literal[True] | bool = ...,
+    ) -> VideoNodeT: ...
 
-    :param clip_a:      Original clip.
-    :param clip_b:      Replacement clip.
-    :param ranges:      Callback to replace clip_a (original clip) with clip_b (replacement clip).
-                        Must return True to replace a with b.
-    :param mismatch:    Accept format or resolution mismatch between clips.
+    @overload
+    def __call__(
+        self, clip_a: vs.VideoNode, clip_b: vs.VideoNode, ranges: _RangesCallBack, *, mismatch: bool = False
+    ) -> vs.VideoNode: ...
 
-    :return:            Clip with ranges from clip_a replaced with clip_b.
-    """
+    @overload
+    def __call__(
+        self,
+        clip_a: vs.VideoNode,
+        clip_b: vs.VideoNode,
+        ranges: _RangesCallBackF[vs.VideoFrame] | _RangesCallBackNF[vs.VideoFrame],
+        *,
+        mismatch: bool = False,
+        prop_src: vs.VideoNode,
+    ) -> vs.VideoNode: ...
 
+    @overload
+    def __call__(
+        self,
+        clip_a: vs.VideoNode,
+        clip_b: vs.VideoNode,
+        ranges: _RangesCallBackF[Sequence[vs.VideoFrame]] | _RangesCallBackNF[Sequence[vs.VideoFrame]],
+        *,
+        mismatch: bool = False,
+        prop_src: Sequence[vs.VideoNode],
+    ) -> vs.VideoNode: ...
 
-@overload
-def replace_ranges(
-    clip_a: vs.VideoNode,
-    clip_b: vs.VideoNode,
-    ranges: _RangesCallBackF[vs.VideoFrame] | _RangesCallBackNF[vs.VideoFrame],
-    *,
-    mismatch: bool = False,
-    prop_src: vs.VideoNode,
-) -> vs.VideoNode:
-    """
-    Replaces frames in a clip on-the-fly with a callback.
+    @overload
+    def __call__(
+        self,
+        clip_a: vs.VideoNode,
+        clip_b: vs.VideoNode,
+        ranges: FrameRangeN | FrameRangesN | _RangesCallBackT | None,
+        exclusive: bool | None = None,
+        mismatch: bool = False,
+        *,
+        prop_src: vs.VideoNode | Sequence[vs.VideoNode] | None = None,
+    ) -> vs.VideoNode: ...
 
-    Example usage:
-    ```py
-    # Replace frames from ``clip_a`` with ``clip_b`` if the picture type of ``clip_a`` is P.
-    clip = replace_ranges(clip_a, clip_b, lambda f: get_prop(f, "_PictType", str) == "P", prop_src=clip_a)
-
-    # Replace frames from ``clip_a`` with ``clip_b`` if the picture type of ``clip_a`` is P
-    # and if the frame number is between 200 and 299 (inclusive)
-    clip = replace_ranges(
-        clip_a, clip_b, lambda n, f: get_prop(f, "_PictType", str) == "P" and n in range(200, 300), prop_src=clip_a
-    )
-    ```
-
-    :param clip_a:      Original clip.
-    :param clip_b:      Replacement clip.
-    :param ranges:      Callback to replace clip_a (original clip) with clip_b (replacement clip).
-                        Must return True to replace a with b.
-    :param mismatch:    Accept format or resolution mismatch between clips.
-    :param prop_src:    Source clip to use for frame properties in the callback.
-
-    :return:            Clip with ranges from clip_a replaced with clip_b.
-    """
-
-
-@overload
-def replace_ranges(
-    clip_a: vs.VideoNode,
-    clip_b: vs.VideoNode,
-    ranges: _RangesCallBackF[Sequence[vs.VideoFrame]] | _RangesCallBackNF[Sequence[vs.VideoFrame]],
-    *,
-    mismatch: bool = False,
-    prop_src: Sequence[vs.VideoNode],
-) -> vs.VideoNode:
-    """
-    Replaces frames in a clip on-the-fly with a callback.
-
-    Example usage:
-    ```py
-    prop_srcs: list[vs.VideNode]()
-    ...
-
-    # Replace frames from ``clip_a`` with ``clip_b`` if the picture type of all the ``prop_srcs`` is P.
-    clip = replace_ranges(
-        clip_a, clip_b, lambda f: all(get_prop(frame, "_PictType", str) == "P" for frame in f), prop_src=prop_srcs
-    )
-
-    # Replace frames from ``clip_a`` with ``clip_b`` if the picture type of all the ``prop_srcs`` is P
-    # and if the frame number is between 200 and 299 (inclusive)
-    clip = replace_ranges(
-        clip_a,
-        clip_b,
-        lambda n, f: all(get_prop(frame, "_PictType", str) == "P" for frame in f) and n in range(200, 300),
-        prop_src=prop_srcs,
-    )
-    ```
-
-    :param clip_a:      Original clip.
-    :param clip_b:      Replacement clip.
-    :param ranges:      Callback to replace clip_a (original clip) with clip_b (replacement clip).
-                        Must return True to replace a with b.
-    :param mismatch:    Accept format or resolution mismatch between clips.
-    :param prop_src:    Source clips to use for frame properties in the callback.
-
-    :return:            Clip with ranges from clip_a replaced with clip_b.
-    """
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        return self._func(*args, **kwargs)
 
 
-@overload
 def replace_ranges(
     clip_a: vs.VideoNode,
     clip_b: vs.VideoNode,
@@ -259,25 +157,41 @@ def replace_ranges(
 ) -> vs.VideoNode:
     """
     Replaces frames in a clip, either with pre-calculated indices or on-the-fly with a callback.
-    Frame ranges are inclusive. This behaviour can be changed by setting `exclusive=True`.
+
+    Frame ranges are inclusive by default.
+    This behaviour can be changed by setting `exclusive=True` for one-time use,
+    or set `replace_ranges.exclusive = True` to apply the change globally.
 
     Examples with clips ``black`` and ``white`` of equal length:
-        * ``replace_ranges(black, white, [(0, 1)])``: replace frames 0 and 1 with ``white``
-        * ``replace_ranges(black, white, [(None, None)])``: replace the entire clip with ``white``
-        * ``replace_ranges(black, white, [(0, None)])``: same as previous
-        * ``replace_ranges(black, white, [(200, None)])``: replace 200 until the end with ``white``
-        * ``replace_ranges(black, white, [(200, -1)])``: replace 200 until the end with ``white``,
-                                                         leaving 1 frame of ``black``
+        ``` py
+        # Replaces frames 0 and 1 with ``white``
+        replace_ranges(black, white, [(0, 1)])
+
+        # Replaces the entire clip with ``white``
+        replace_ranges(black, white, [(None, None)])
+
+        # Same as previous
+        replace_ranges(black, white, [(0, None)])
+
+        # Replaces 200 until the end with ``white``
+        replace_ranges(black, white, [(200, None)])
+
+        # Replaces 200 until the end with ``white``, leaving 1 frame of ``black``
+        replace_ranges(black, white, [(200, -1)])
+        ```
 
     A callback function can be used to replace frames based on frame properties.
     The function must return a boolean value.
 
     Example of using a callback function:
-        * ``replace_ranges(clip_a, clip_b, lambda f: get_prop(f, '_PictType', str) == 'P', prop_src=clip_a)``:
-          Replace frames from ``clip_a`` with ``clip_b`` if the picture type of ``clip_a`` is P.
+        ```py
+        # Replaces frames from ``clip_a`` with ``clip_b`` if the picture type of ``clip_a`` is P.
+        replace_ranges(clip_a, clip_b, lambda f: get_prop(f, '_PictType', str) == 'P', prop_src=clip_a)``
+        ```
 
     Optional Dependencies:
-        * `vs-zip <https://github.com/dnjulek/vapoursynth-zip>`_ (highly recommended!)
+
+    - [vs-zip](https://github.com/dnjulek/vapoursynth-zip) (highly recommended!)
 
     :param clip_a:      Original clip.
     :param clip_b:      Replacement clip.
@@ -298,17 +212,6 @@ def replace_ranges(
 
     :return:            Clip with ranges from clip_a replaced with clip_b.
     """
-
-
-def replace_ranges(
-    clip_a: vs.VideoNode,
-    clip_b: vs.VideoNode,
-    ranges: FrameRangeN | FrameRangesN | _RangesCallBackT | None,
-    exclusive: bool = False,
-    mismatch: bool = False,
-    *,
-    prop_src: vs.VideoNode | Sequence[vs.VideoNode] | None = None,
-) -> vs.VideoNode:
     from ..functions import invert_ranges, normalize_ranges
 
     if (ranges != 0 and not ranges) or clip_a is clip_b:
@@ -357,8 +260,8 @@ def replace_ranges(
 
     a_ranges = invert_ranges(clip_a, clip_b, b_ranges, exclusive)
 
-    a_trims = [clip_a[max(0, start - exclusive):end + (not exclusive)] for start, end in a_ranges]
-    b_trims = [clip_b[start:end + (not exclusive)] for start, end in b_ranges]
+    a_trims = [clip_a[max(0, start - exclusive) : end + (not exclusive)] for start, end in a_ranges]
+    b_trims = [clip_b[start : end + (not exclusive)] for start, end in b_ranges]
 
     if a_ranges:
         main, other = (a_trims, b_trims) if (a_ranges[0][0] == 0) else (b_trims, a_trims)
