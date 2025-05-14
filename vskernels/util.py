e597511231df116baf60a46ab2c728ef8267c472
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-05-14 04:38:04 +0200

    update LinearLight classes

--- vskernels/util.py
+++ vskernels/util.py
@@ -1,20 +1,23 @@
 from __future__ import annotations
 
+from contextlib import AbstractContextManager
 from dataclasses import dataclass
+from functools import wraps
 from math import exp
-from typing import TYPE_CHECKING, Any, ClassVar, cast
+from typing import TYPE_CHECKING, Any, Callable, Concatenate
 
+from jetpytools import P
 from typing_extensions import Self
 
 from vsexprtools import norm_expr
 from vstools import (
-    ConstantFormatVideoNode, CustomRuntimeError, CustomValueError, HoldsVideoFormatT, InvalidTransferError, Matrix,
-    MatrixT, Transfer, VideoFormatT, cachedproperty, depth, get_video_format, vs
+    CustomRuntimeError, CustomValueError, HoldsVideoFormatT, Matrix, MatrixT, Transfer, VideoFormatT, cachedproperty,
+    check_variable_format, depth, get_video_format, vs, vs_object
 )
 
-from .abstract import Kernel, KernelT, Resampler, ResamplerT, Scaler
-from .kernels import Bicubic, Catrom, Point
-from .types import Center, LeftShift, Slope, TopShift
+from .abstract import Resampler, ResamplerT
+from .kernels import Catrom, Point
+from .types import Center, Slope
 
 __all__ = [
     'LinearLight',
@@ -23,75 +26,107 @@ __all__ = [
 ]
 
 
-    @dataclass
-    class LinearLightProcessing(cachedproperty.baseclass):
-        ll: LinearLight
+@dataclass
+class LinearLightProcessing(cachedproperty.baseclass):
+    ll: LinearLight
 
-        def get_linear(self) -> vs.VideoNode:
-            wclip: vs.VideoNode = self.ll._wclip
+    def get_linear(self) -> vs.VideoNode:
+        """Getter for `linear` cached property."""
+        wclip = self.ll._resampler.resample(
+            self.ll._wclip,
+            vs.RGBS if self.ll._wclip.format.color_family in (vs.YUV, vs.RGB) else vs.GRAYS,
+            matrix_in=self.ll._matrix,
+            transfer_in=self.ll._curve,
+            transfer=Transfer.LINEAR
+        )
 
-            if self.ll._wclip.format.color_family is vs.YUV:
-                wclip = self.ll._resampler.resample(wclip, vs.RGBS, None, self.ll._matrix)
-            else:
-                wclip = depth(wclip, 32)
+        if self.ll.sigmoid:
+            wclip = norm_expr(
+                wclip,
+                '{center} 1 {slope} / 1 x 0 max 1 min {scale} * {offset} + / 1 - log * -',
+                center=self.ll._scenter, slope=self.ll._sslope,
+                scale=self.ll._sscale, offset=self.ll._soffset,
+                func=self.__class__
+            )
 
-            if self.ll.linear:
-                wclip = Point.scale_function(wclip, transfer_in=self.ll._curve, transfer=Transfer.LINEAR)
+        return wclip
 
-            if self.ll.sigmoid:
-                if Transfer.from_video(wclip, func=self.__class__) in (Transfer.ST2084, Transfer.STD_B67):
-                    raise InvalidTransferError(
-                        self.__class__, Transfer.from_video(wclip, func=self.__class__),
-                        'Sigmoid scaling is not supported with HDR!'
-                    )
+    def set_linear(self, processed: vs.VideoNode) -> None:
+        """Setter for `linear` cached property."""
+        if self.ll._exited:
+            raise CustomRuntimeError(
+                'You can\'t set .linear after going out of the context manager!', func=self.__class__
+            )
+        self._linear = processed
 
-                wclip = norm_expr(
-                    wclip,
-                    '{center} 1 {slope} / 1 x 0 max 1 min {scale} * {offset} + / 1 - log * -',
-                    center=self.ll._scenter, slope=self.ll._sslope,
-                    scale=self.ll._sscale, offset=self.ll._soffset,
-                    func=self.__class__
-                )
+    linear = cachedproperty[[Self], vs.VideoNode, Self, vs.VideoNode, ...](get_linear, set_linear)
+    """Cached property to use for linear light processing."""
 
-            return wclip
+    @cachedproperty
+    def out(self) -> vs.VideoNode:
+        if not self.ll._exited:
+            raise CustomRuntimeError(
+                'You can\'t get .out while still inside of the context manager!', func=self.__class__
+            )
 
-        def set_linear(self, processed: vs.VideoNode) -> None:
-            if self.ll._exited:
-                raise CustomRuntimeError(
-                    'You can\'t set .linear after going out of the context manager!', func=self.__class__
-                )
-            self._linear = processed
+        if not hasattr(self, '_linear'):
+            raise CustomValueError('You need to set .linear before getting .out!', self.__class__)
 
-        linear = cachedproperty[[Self], vs.VideoNode, Self, vs.VideoNode, ...](get_linear, set_linear)
 
-        @cachedproperty
-        def out(self) -> vs.VideoNode:
-            if not self.ll._exited:
-                raise CustomRuntimeError(
-                    'You can\'t get .out while still inside of the context manager!', func=self.__class__
-                )
-
-            if not hasattr(self, '_linear'):
-                raise CustomValueError('You need to set .linear before getting .out!', self.__class__)
-
+        if self.ll.sigmoid:
+            processed = norm_expr(
+                self._linear,
+                '1 1 {slope} {center} x 0 max 1 min - * exp + / {offset} - {scale} /',
+                slope=self.ll._sslope, center=self.ll._scenter,
+                offset=self.ll._soffset, scale=self.ll._sscale,
+                func=self.__class__
+            )
+        else:
             processed = self._linear
 
-            if self.ll.sigmoid:
-                processed = norm_expr(
-                    processed,
-                    '1 1 {slope} {center} x 0 max 1 min - * exp + / {offset} - {scale} /',
-                    slope=self.ll._sslope, center=self.ll._scenter,
-                    offset=self.ll._soffset, scale=self.ll._sscale,
-                    func=self.__class__
-                )
+        processed = vs.core.resize2.Point(processed, transfer_in=Transfer.LINEAR, transfer=self.ll._curve)
 
-            if self.ll.linear:
-                processed = Point.scale_function(processed, transfer_in=Transfer.LINEAR, transfer=self.ll._curve)
+        return resample_to(processed, self.ll._fmt, self.ll._matrix, self.ll._resampler)
 
-            return resample_to(processed, self.ll._fmt, self.ll._matrix, self.ll._resampler)
+
+@dataclass
+class LinearLight(AbstractContextManager[LinearLightProcessing], vs_object):
+    """Utility class for processing a clip in linear format."""
+
+    clip: vs.VideoNode
+    """Input clip."""
+
+    sigmoid: bool | tuple[Slope, Center] = False
+    """
+    Whether to use sigmoid transfer curve. Can be True, False, or a tuple of (slope, center).
+    `True` applies the defaults values (6.5, 0.75).
+    Keep in mind sigmoid slope has to be in range 1.0-20.0. (inclusive)
+    and sigmoid center has to be in range 0.0-1.0 (inclusive).
+    """
+
+    resampler: ResamplerT | None = Catrom
+    """Resampler for converting to linear format and converting back to input clip format."""
+
+    out_fmt: int | VideoFormatT | HoldsVideoFormatT | None = None
+    """Optional output format."""
+
+    def from_func(
+        self, func: Callable[Concatenate[vs.VideoNode, P], vs.VideoNode]
+    ) -> Callable[Concatenate[vs.VideoNode, P], vs.VideoNode]:
+        """
+        Decorator version of LinearLight
+        """
+
+        @wraps(func)
+        def _wrapped(clip: vs.VideoNode, *args: P.args, **kwargs: P.kwargs) -> vs.VideoNode:
+            with self as ll:
+                ll.linear = func(clip, *args, **kwargs)
+            return ll.out
+
+        return _wrapped
 
     def __enter__(self) -> LinearLightProcessing:
-        self.linear = self.linear or not not self.sigmoid
+        assert check_variable_format(self.clip, self.__class__)
 
         if self.sigmoid is not False:
             if self.sigmoid is True:
@@ -99,30 +134,35 @@ __all__ = [
 
             self._sslope, self._scenter = self.sigmoid
 
-            if 1.0 > self._sslope or self._sslope > 20.0:
+            if not 1.0 <= self._sslope <= 20.0:
                 raise CustomValueError('sigmoid slope has to be in range 1.0-20.0 (inclusive).', self.__class__)
 
-            if 0.0 > self._scenter or self._scenter > 1.0:
+            if not 0.0 <= self._scenter <= 1.0:
                 raise CustomValueError('sigmoid center has to be in range 0.0-1.0 (inclusive).', self.__class__)
 
             self._soffset = 1.0 / (1 + exp(self._sslope * self._scenter))
             self._sscale = 1.0 / (1 + exp(self._sslope * (self._scenter - 1))) - self._soffset
 
-        _fmt = self.out_fmt or self.clip.format
-        assert _fmt
-        self._fmt = _fmt
+        self._fmt = self.out_fmt or self.clip.format
 
-        self._wclip = cast(ConstantFormatVideoNode, depth(self.clip, 32) if self.sigmoid else self.clip)
+        self._wclip = self.clip
         self._curve = Transfer.from_video(self.clip)
         self._matrix = Matrix.from_video(self.clip)
         self._resampler = Catrom.ensure_obj(self.resampler)
 
         self._exited = False
 
-        return self.LinearLightProcessing(self)
+        return LinearLightProcessing(self)
 
     def __exit__(self, *args: Any, **kwargs: Any) -> None:
         self._exited = True
+
+    def __vs_del__(self, core_id: int) -> None:
+        if not TYPE_CHECKING:
+            self.clip = None
+            self.out_fmt = None
+            self._fmt = None
+            self._wclip = None
 
 
 def resample_to(
