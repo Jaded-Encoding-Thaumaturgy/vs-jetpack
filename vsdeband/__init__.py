c12febbf12b9879eca7c5e3f38a8196a51363985
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-05-26 04:04:48 +0200

    delete funcs module

--- vsdeband/__init__.py
+++ vsdeband/__init__.py
@@ -2,9 +2,6 @@
 
 from .abstract import *
 from .f3kdb import *
-from .filters import *
-from .funcs import *
 from .mask import *
 from .noise import *
 from .placebo import *
-from .types import *
