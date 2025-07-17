ba41e609940131445a797fad2907dbfa1c5c4ad8
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-07-17 17:28:52 +0200

    edge_cleaner, YAHR: use awarpsharp wrapper from vsrgtools

--- vsdehalo/warp.py
+++ vsdehalo/warp.py
@@ -10,15 +10,13 @@ from typing import Sequence
 
 from vsexprtools import norm_expr
 from vsmasktools import EdgeDetect, EdgeDetectT, Morpho, PrewittStd
-from vsrgtools import BlurMatrix, box_blur, min_blur, remove_grain, repair
+from vsrgtools import BlurMatrix, awarpsharp, box_blur, min_blur, remove_grain, repair
 from vsrgtools.rgtools import Repair
 from vstools import (
-    FunctionUtil,
     PlanesT,
     cround,
     get_y,
     limiter,
-    padder,
     scale_mask,
     vs,
 )
@@ -50,65 +48,58 @@ def edge_cleaner(
     Returns:
         The processed video clip with cleaned edges.
     """
-    func = FunctionUtil(clip, edge_cleaner, planes, (vs.YUV, vs.GRAY), (8, 16))
-
     edgemask = EdgeDetect.ensure_obj(edgemask, edge_cleaner)
 
     if smode:
         strength += 4
 
-    padded = padder.MIRROR(func.work_clip, 6, 6, 6, 6)
-    warped = padded.warp.AWarpSharp2(blur=1, depth=cround(strength / 2), planes=planes)
-    warped = warped.std.Crop(6, 6, 6, 6)
+    warped = awarpsharp(clip, blur=2, depth=cround(strength / 2), planes=planes)
+    warped = repair(warped, clip, rmode, planes)
 
-    warped = repair(warped, func.work_clip, rmode, func.norm_planes)
-
-    y_mask = get_y(func.work_clip)
+    y_mask = get_y(clip)
 
     mask = norm_expr(
         edgemask.edgemask(y_mask),
         "x {sc4} < 0 x {sc32} > range_in_max x ? ?",
-        sc4=scale_mask(4, 8, func.work_clip),
-        sc32=scale_mask(32, 8, func.work_clip),
+        sc4=scale_mask(4, 8, clip),
+        sc32=scale_mask(32, 8, clip),
         func=edge_cleaner,
     )
     mask = box_blur(mask)
 
-    final = warped.std.MaskedMerge(func.work_clip, mask)
+    final = warped.std.MaskedMerge(clip, mask, planes)
 
     if hot:
-        final = repair(final, func.work_clip, 2)
+        final = repair(final, clip, 2, planes)
 
     if smode:
         clean = remove_grain(y_mask, 17)
 
         diff = y_mask.std.MakeDiff(clean)
 
-        mask = edgemask.edgemask(
-            diff.std.Levels(scale_mask(40, 8, func.work_clip), scale_mask(168, 8, func.work_clip), 0.35)
-        )
+        mask = edgemask.edgemask(diff.std.Levels(scale_mask(40, 8, clip), scale_mask(168, 8, clip), 0.35))
         mask = norm_expr(
             remove_grain(mask, 7),
             "x {sc4} < 0 x {sc16} > range_in_max x ? ?",
-            sc4=scale_mask(4, 8, func.work_clip),
-            sc16=scale_mask(16, 8, func.work_clip),
+            sc4=scale_mask(4, 8, clip),
+            sc16=scale_mask(16, 8, clip),
             func=edge_cleaner,
         )
 
-        final = final.std.MaskedMerge(func.work_clip, mask)
+        final = final.std.MaskedMerge(clip, mask, planes)
 
-    return func.return_clip(final)
+    return final
 
 
 def YAHR(  # noqa: N802
-    clip: vs.VideoNode, blur: int = 2, depth: int | Sequence[int] = 32, expand: float = 5, planes: PlanesT = 0
+    clip: vs.VideoNode, blur: int = 3, depth: int | Sequence[int] = 32, expand: float = 5, planes: PlanesT = 0
 ) -> vs.VideoNode:
     """
     Applies YAHR (Yet Another Halo Remover) to reduce halos in a video clip.
 
     Args:
         clip: The input video clip to process.
-        blur: The blur strength for the warping process. Default is 2.
+        blur: The blur strength for the warping process. Default is 3.
         depth: The depth of the warping process. Default is 32.
         expand: The expansion factor for edge detection. Default is 5.
         planes: The planes to process. Default is 0 (luma only).
@@ -116,24 +107,18 @@ def YAHR(  # noqa: N802
     Returns:
         The processed video clip with reduced halos.
     """
-    func = FunctionUtil(clip, YAHR, planes, (vs.YUV, vs.GRAY), (8, 16))
-
-    padded = padder.MIRROR(func.work_clip, 6, 6, 6, 6)
-    warped = padded.warp.AWarpSharp2(blur=blur, depth=depth, planes=func.norm_planes)
-    warped = warped.std.Crop(6, 6, 6, 6)
+    warped = awarpsharp(clip, blur=blur, depth=depth, planes=planes)
 
     blur_diff, blur_warped_diff = [
-        c.std.MakeDiff(
-            BlurMatrix.BINOMIAL()(min_blur(c, 2, planes=func.norm_planes), planes=func.norm_planes), func.norm_planes
-        )
-        for c in (func.work_clip, warped)
+        c.std.MakeDiff(BlurMatrix.BINOMIAL()(min_blur(c, 2, planes=planes), planes=planes), planes)
+        for c in (clip, warped)
     ]
 
-    rep_diff = repair(blur_diff, blur_warped_diff, 13, func.norm_planes)
+    rep_diff = repair(blur_diff, blur_warped_diff, 13, planes)
 
-    yahr = func.work_clip.std.MakeDiff(blur_diff.std.MakeDiff(rep_diff, func.norm_planes), func.norm_planes)
+    yahr = clip.std.MakeDiff(blur_diff.std.MakeDiff(rep_diff, planes), planes)
 
-    y_mask = get_y(func.work_clip)
+    y_mask = get_y(clip)
 
     v_edge = norm_expr([y_mask, Morpho.maximum(y_mask, iterations=2)], "y x - 8 range_max * 255 / - 128 *", func=YAHR)
 
@@ -143,6 +128,6 @@ def YAHR(  # noqa: N802
 
     mask = limiter(norm_expr([mask1, mask2], "x 16 * range_max y - min", func=YAHR))
 
-    final = func.work_clip.std.MaskedMerge(yahr, mask, func.norm_planes)
+    final = clip.std.MaskedMerge(yahr, mask, planes)
 
-    return func.return_clip(final)
+    return final
