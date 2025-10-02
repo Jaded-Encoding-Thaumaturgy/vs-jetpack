Jaded-Encoding-Thaumaturgy/vs-jetpack

commit d72222b433236d9407a837e07641841f049160d7

PackageStorage: remove dead code

vstools/functions/file.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import inspect
 from typing import TYPE_CHECKING
 
 from jetpytools import CustomRuntimeError, SPath, SPathLike, get_script_path
@@ -13,8 +12,10 @@ class PackageStorage:
 
     def __init__(self, cwd: SPathLike | None = None, *, mode: int = 0o777, package_name: str | None = None) -> None:
         if not package_name:
-            frame = inspect.stack()[1]
-            module = inspect.getmodule(frame[0])
+            from inspect import getmodule, stack
+
+            frame = stack()[1]
+            module = getmodule(frame[0])
 
             if module:
                 package_name = module.__name__
@@ -28,27 +29,12 @@ class PackageStorage:
         package_name = package_name.strip(".").split(".")[0]
 
         if not cwd:
-            cwd = SPath(get_script_path())
+            cwd = get_script_path()
         elif not isinstance(cwd, SPath):
-            cwd = SPath(str(cwd))
-
-        cwd = cwd.get_folder()
-        base_folder = cwd / self.BASE_FOLDER
-
-        for old_names in (".vsstg", ".vsiew"):
-            old_base_folder = cwd / ".vsstg"
-
-            if old_base_folder.exists():
-                old_base_folder.move_dir(base_folder)
-
-        old_folder = cwd / f".{package_name}"
-        new_folder = base_folder / package_name
-
-        if old_folder.exists():
-            old_folder.move_dir(new_folder)
+            cwd = SPath(cwd)
 
         self.mode = mode
-        self.folder = new_folder
+        self.folder = cwd / self.BASE_FOLDER / package_name
 
     def ensure_folder(self) -> None:
         self.folder.mkdir(self.mode, True, True)

commit 15041e0f7afd691b80c533aa8ebbfbdd84ee33a6

PackageStorage: fix folder setup when script path is used
Regression by d72222b. `cwd` may point to a file (if coming from the
`get_script_path()` branch) or a directory. There used to be a
`cwd = cwd.get_folder()` here to normalize it to a directory, but it was
removed. What happens then is that `cwd` may be
e.g. `/path/to/encode/01.py`, and then the constructed folder path ends
up being `/path/to/encode/01.py/.vsjet`, which won't work.

vstools/functions/file.py
@@ -34,7 +34,7 @@ class PackageStorage:
             cwd = SPath(cwd)
 
         self.mode = mode
-        self.folder = cwd / self.BASE_FOLDER / package_name
+        self.folder = cwd.get_folder() / self.BASE_FOLDER / package_name
 
     def ensure_folder(self) -> None:
         self.folder.mkdir(self.mode, True, True)

tests/vstools/functions/test_file.py
@@ -31,3 +31,10 @@ def test_package_storage_get_file(storage: PackageStorage) -> None:
     result = storage.get_file("test", ext=PurePath("should-use-my-extension.mkv"))
     assert result.name == "test.mkv"
     assert result.suffix == ".mkv"
+
+
+def test_package_storage_default_folder(storage: PackageStorage) -> None:
+    # Have `cwd` be a file just to pretend it's the result of `get_script_path()`.
+    storage = PackageStorage(cwd=PurePath(__file__))
+
+    assert storage.folder.is_dir()