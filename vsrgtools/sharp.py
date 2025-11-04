905b682422f9d22adb72b7b355148773ade10291
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-11-04 18:48:36 +0100

    update FunctionUtil usage

--- vsrgtools/sharp.py
+++ vsrgtools/sharp.py
@@ -17,6 +17,7 @@ from vstools import (
     VSFunctionNoArgs,
     VSFunctionPlanesArgs,
     check_ref_clip,
+    normalize_param_planes,
     scale_delta,
     scale_mask,
     vs,
@@ -187,7 +188,7 @@ def fine_sharp(
     if xstr:
         xysharp = norm_expr([sharp, box_blur(sharp, planes=planes)], "x x y - 9.9 * +", planes, func=func.func)
         rpsharp = repair(xysharp, sharp, 12, planes)
-        sharp = sharp.std.Merge(rpsharp, func.norm_seq(xstr, 0))
+        sharp = sharp.std.Merge(rpsharp, normalize_param_planes(rpsharp, xstr, func.planes, 0))
 
     return func.return_clip(sharp)
 
