d56456c3ab7a4434e701fe1620c8eaf5f17885e8
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-11-04 22:15:31 +0100

    bm3d: remove down conversions

--- vsdenoise/blockmatch.py
+++ vsdenoise/blockmatch.py
@@ -718,7 +718,7 @@ def bm3d(
             col_fam=vs.RGB,
         )
 
-        return depth(denoised, clip)
+        return denoised
 
     preclip = depth(clip, 32)
     prepre = depth(pre, 32) if pre else pre
@@ -747,7 +747,7 @@ def bm3d(
 
         denoised = _bm3d_wolfram(preclip, prepre, preref, chroma=True)
 
-        return depth(denoised, clip)
+        return denoised
 
     # GRAY or subsampled YUV
     if backend == BM3D.Backend.OLD:
@@ -756,4 +756,4 @@ def bm3d(
 
     denoised = _bm3d_wolfram(preclip, prepre, preref)
 
-    return depth(denoised, clip)
+    return denoised
