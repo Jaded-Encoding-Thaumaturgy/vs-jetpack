15492143b5c2b037f46ec192fcecd2c268771aa2
Ichunjo <ichunjo.le.terrible@gmail.com>
2026-03-22 00:11:42 +0100

    fix PYI066

--- vsjetpack/types.py
+++ vsjetpack/types.py
@@ -2,12 +2,12 @@ import sys
 
 __all__ = ["TypeIs", "TypeVar", "deprecated"]
 
-if sys.version_info < (3, 13):
+if sys.version_info >= (3, 13):
+    from typing import TypeIs, TypeVar
+    from warnings import deprecated
+else:
     import typing_extensions
 
     TypeIs = typing_extensions.TypeIs
     TypeVar = typing_extensions.TypeVar
     deprecated = typing_extensions.deprecated
-else:
-    from typing import TypeIs, TypeVar
-    from warnings import deprecated
