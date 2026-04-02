bcc5c8d07c96ac9b1f340aa61891d74075061a20
Ichunjo <ichunjo.le.terrible@gmail.com>
2026-04-02 16:25:44 +0200

    PackageStorage: fallback to SPath.cwd() when get_script_path raises an error

--- vstools/utils/file.py
+++ vstools/utils/file.py
@@ -1,8 +1,13 @@
 from __future__ import annotations
 
-from jetpytools import SPath, SPathLike, get_script_path
+from logging import getLogger
+
+from jetpytools import CustomRuntimeError, SPath, SPathLike, get_script_path
 
 __all__ = ["PackageStorage"]
+
+
+logger = getLogger(__name__)
 
 
 class PackageStorage:
@@ -11,13 +16,18 @@ class PackageStorage:
     def __init__(self, cwd: SPathLike | None = None, *, package_name: str, mode: int = 0o777) -> None:
         package_name = package_name.strip(".").split(".")[0]
 
-        if not cwd:
-            cwd = get_script_path()
-        elif not isinstance(cwd, SPath):
+        if cwd is not None:
             cwd = SPath(cwd)
+        else:
+            try:
+                cwd = get_script_path()
+            except CustomRuntimeError:
+                cwd = SPath.cwd()
 
         self.mode = mode
         self.folder = cwd.get_folder() / self.BASE_FOLDER / package_name
+
+        logger.debug("PackageStorage(%s) initialized in the %r directory ", package_name, self.folder)
 
     def ensure_folder(self) -> None:
         self.folder.mkdir(self.mode, True, True)
