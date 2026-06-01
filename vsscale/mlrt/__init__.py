7460fc4526401d969864216466017564873d77e6
Vardë <ichunjo.le.terrible@gmail.com>
2026-06-01 17:36:01 +0200

    refactor onnx submodule
    - Link the new mlrt submodule to this onnx submodule
    - Simplify dynamic resolution path
    - Re-implement calc_tilesize from vsmlrt
    - Remove deprecated ArtCNN models (C4F32_Chroma, C16F64, C16F64_Chroma, C16F64_DS, R16F96_Chroma)
    - Remove older Waifu2x models (UpConv7 suite )

--- vsscale/mlrt/__init__.py
+++ vsscale/mlrt/__init__.py
@@ -1,3 +1,17 @@
 from .backend import Backend
+from .settings import (
+    get_artifacts_folder,
+    get_global_cache,
+    get_local_cache,
+    get_model_folder,
+    get_onnx_folder,
+)
 
-__all__ = ["Backend"]
+__all__ = [
+    "Backend",
+    "get_artifacts_folder",
+    "get_global_cache",
+    "get_local_cache",
+    "get_model_folder",
+    "get_onnx_folder",
+]
