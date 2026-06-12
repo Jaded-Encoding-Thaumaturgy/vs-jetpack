Jaded-Encoding-Thaumaturgy/vs-jetpack

commit 288f241eedf79768a4895e85c7eff2cde8051eca

Correct limiter usage

vsmasktools/spat_funcs.py
@@ -14,7 +14,6 @@ from vstools import (
     get_peak_value,
     get_sample_type,
     get_y,
-    limiter,
     plane,
     scale_value,
     vs,
@@ -100,7 +99,6 @@ def adg_mask(
     return scaled_clips[0]
 
 
-@limiter
 def retinex(
     clip: vs.VideoNode,
     sigma: Sequence[float] = [25, 80, 250],

vsmasktools/details.py
@@ -41,7 +41,7 @@ def detail_mask(
     return mask
 
 
-@limiter
+@limiter(mask=True)
 def detail_mask_neo(
     clip: vs.VideoNode,
     sigma: float = 5.0,

vsmasktools/hardsub.py
@@ -313,7 +313,6 @@ class HardsubSign(HardsubMask):
         self.expand_mode = expand_mode
         super().__init__(ranges, bound, blur=blur, refframes=refframes)
 
-    @limiter(mask=True)
     def _mask(self, clip: vs.VideoNode, ref: vs.VideoNode, **kwargs: Any) -> vs.VideoNode:
         hsmf = norm_expr([clip, ref], "x y - abs", func=self.__class__)
         hsmf = core.resize.Bilinear(hsmf, format=clip.format.replace(subsampling_w=0, subsampling_h=0))
@@ -476,7 +475,6 @@ class HardsubASS(HardsubMask):
         self.fontdir = fontdir
         super().__init__(ranges, bound, blur=blur, refframes=refframes)
 
-    @limiter(mask=True)
     def _mask(self, clip: vs.VideoNode, ref: vs.VideoNode, **kwargs: Any) -> vs.VideoNode:
         mask = core.sub.TextFile(ref, self.filename, fontdir=self.fontdir, blend=False).std.PropToClip("_Alpha")
 

vsscale/mask.py
@@ -5,12 +5,11 @@ from jetpytools import iterate
 from vsexprtools import ExprOp, norm_expr
 from vsmasktools import Morpho, XxpandMode
 from vsrgtools import BlurMatrix, box_blur, gauss_blur
-from vstools import ConvMode, core, get_y, limiter, shift_clip_multi, split, vs
+from vstools import ConvMode, core, get_y, shift_clip_multi, split, vs
 
 __all__ = ["descale_detail_mask", "descale_error_mask"]
 
 
-@limiter
 def descale_detail_mask(
     clip: vs.VideoNode, rescaled: vs.VideoNode, thr: float = 0.05, inflate: int = 2, xxpand: tuple[int, int] = (4, 0)
 ) -> vs.VideoNode:
@@ -47,7 +46,6 @@ def descale_detail_mask(
     return mask
 
 
-@limiter
 def descale_error_mask(
     clip: vs.VideoNode,
     rescaled: vs.VideoNode,

commit 30551867273f9daf25a65465008b57a4736bef8a

retinex: re-add limiter
Regression from 288f241eedf79768a4895e85c7eff2cde8051eca

vsmasktools/spat_funcs.py
@@ -14,6 +14,7 @@ from vstools import (
     get_peak_value,
     get_sample_type,
     get_y,
+    limiter,
     plane,
     scale_mask,
     scale_value,
@@ -100,6 +101,7 @@ def adg_mask(
     return scaled_clips[0]
 
 
+@limiter(mask=True)
 def retinex(
     clip: vs.VideoNode,
     sigma: Sequence[float] = [25, 80, 250],