7bff05dee8e94076265f48def2f3bc44bf8fe579
evan <176516814+emotion3459@users.noreply.github.com>
2026-07-09 16:04:01 -0400

    fine_dehalo: reduced intermediate rounding in masks (#328)
    The `SHRINK_EDGES_EXCL` has been removed
    ---------
    
    Co-authored-by: Vardë <ichunjo.le.terrible@gmail.com>

--- vsdehalo/mask.py
+++ vsdehalo/mask.py
@@ -114,7 +114,7 @@ class FineDehalo[**P, R]:
         debugging or further processing.
         """
 
-        _names = ("EDGES", "SHARP_EDGES", "LARGE_EDGES", "IGNORE_DETAILS", "SHRINK", "SHRINK_EDGES_EXCL", "MAIN")
+        _names = ("EDGES", "SHARP_EDGES", "LARGE_EDGES", "IGNORE_DETAILS", "SHRINK", "MAIN")
 
         def __init__(
             self,
@@ -206,11 +206,12 @@ class FineDehalo[**P, R]:
             # Mask growing
             shrink = Morpho.expand(light, rx, ry, XxpandMode.ELLIPSE, planes=planes, func=func)
 
-            # At this point, because the mask was made of a shades of grey, we may
-            # end up with large areas of dark grey after shrinking. To avoid this,
-            # we amplify and saturate the mask here (actually we could even
-            # binarize it).
-            shrink = norm_expr(shrink, "x 4 *", planes, func=func)
+            # At this point, because the mask was made of shades of grey, we may
+            # end up with large areas of dark grey after shrinking.
+            # To avoid this, we binarize the mask at 0.25.
+            # This also prevents float values from escaping the [0, 1] range,
+            # which would break the subsequent box blur smoothing step.
+            shrink = Morpho.binarize_mask(shrink, 0.25, planes=planes)
             shrink = Morpho.inpand(shrink, rx, ry, XxpandMode.ELLIPSE, planes=planes, func=func)
 
             # This mask is almost binary, which will produce distinct
@@ -219,22 +220,20 @@ class FineDehalo[**P, R]:
 
             # Final mask building #
 
-            # Previous mask may be a bit weak on the pure edge side, so we ensure
-            # that the main edges are really excluded. We do not want them to be
-            # smoothed by the halo removal.
-            shr_med = (
-                norm_expr([strong, shrink], tuple("x y max" * ex for ex in exclude), planes, func=func)
-                if any(exclude)
-                else strong
+            # Expression that combines edge exclusion, mask subtraction,
+            # and optional edge processing into a single filter node.
+            # Explanation:
+            # 1. If exclude is True: shr_med = max(strong, shrink), else: shr_med = strong
+            # 2. base_mask = (large - shr_med) * 2    (amplify difference to saturate halo areas)
+            # 3. If edgeproc > 0: mask = base_mask + (base_mask * strong * edgeproc * 2/3) else: mask = base_mask
+            mask = norm_expr(
+                [strong, shrink, large],
+                "{edgeproc} not z {exclude} x y max x ? - 2 * dup x {edgeproc} 2 3 / * * + ?",
+                planes,
+                exclude=map(int, exclude),
+                edgeproc=edgeproc,
+                func=func,
             )
-
-            # Subtracts masks and amplifies the difference to be sure we get 255
-            # on the areas to be processed.
-            mask = norm_expr([large, shr_med], "x y - 2 *", planes, func=func)
-
-            # If edge processing is required, adds the edgemask
-            if any(edgeproc):
-                mask = norm_expr([mask, strong], "x y {edgeproc} 0.66 * * +", planes, func=func, edgeproc=edgeproc)
 
             # Smooth again and amplify to grow the mask a bit, otherwise the halo
             # parts sticking to the edges could be missed.
@@ -246,7 +245,6 @@ class FineDehalo[**P, R]:
             self._large = large
             self._light = light
             self._shrink = shrink
-            self._shr_med = shr_med
             self._main = mask
 
         def __getitem__(self, index: str) -> vs.VideoNode:
@@ -282,10 +280,6 @@ class FineDehalo[**P, R]:
         @property
         def SHRINK(self) -> vs.VideoNode:  # noqa: N802
             return self._shrink
-
-        @property
-        def SHRINK_EDGES_EXCL(self) -> vs.VideoNode:  # noqa: N802
-            return self._shr_med
 
         @property
         def MAIN(self) -> vs.VideoNode:  # noqa: N802
