a14d26ca66b4794277a3dac619faa88b272307be
emotion3459 <176516814+emotion3459@users.noreply.github.com>
2026-08-30 19:50:20 -0400

    mask_shimmer: remove incorrect early exit

--- vsdeinterlace/qtgmc.py
+++ vsdeinterlace/qtgmc.py
@@ -1360,7 +1360,7 @@ class QTGMCGraph(VSObject):
 
     @cachedproperty
     def _motion_blur_level(self) -> float:
-        if self.settings.motion_blur_shutter_angle is False:
+        if not self.settings.motion_blur_shutter_angle:
             return 0
 
         angle_in, angle_out = self.settings.motion_blur_shutter_angle
@@ -1786,9 +1786,6 @@ def mask_shimmer(
         Clip with only bob shimmer fixes kept.
     """
     func = func or mask_shimmer
-
-    if not erosion_distance:
-        return flt
 
     ed1 = 1 + erosion_distance // 3
     ed2 = (erosion_distance + 4) // 3
