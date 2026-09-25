478eb041bac1b7ce3ff58ef55d33f20b2a4a9ea1
Vardë <Ichunjo@users.noreply.github.com>
2026-09-25 21:28:29 +0200

    Drop Python 3.12 support (#396)
    - Drop Python 3.12 support in accordance with the SPEC 0 policy
    - Bump minimum NumPy version to 2.5.0
    - Bump minimum SciPy version to 1.18.0
    - Modernize some type annotations

--- vsjetpack/types.py
+++ vsjetpack/types.py
@@ -1,12 +1,8 @@
 import sys
+from typing import TypeIs, TypeVar
+from warnings import deprecated
 
 __all__ = ["TypeForm", "TypeIs", "TypeVar", "deprecated"]
-
-if sys.version_info < (3, 13):
-    from typing_extensions import TypeIs, TypeVar, deprecated
-else:
-    from typing import TypeIs, TypeVar
-    from warnings import deprecated
 
 if sys.version_info < (3, 15):
     from typing_extensions import TypeForm
